9b217ac1f7803f421c0dbd749d0c31a521a54b05
Rebecca Van Caeneghem <Rebecca.Van.Caeneghem@ad.core.vdabantwerpen.intra>
2019-05-09 15:54:37 +0200

    foto

--- testInvoer.js
+++ testInvoer.js
@@ -1,43 +1,74 @@
 const rooturl = "https://scrumserver.tenobe.org/scrum/api";
 let url=rooturl+'/profiel/create.php';
 
+function uploadPicture(base64String) {
+    console.log('     • Foto wordt doorgestuurd naar de API.');
+    let naam = 'foto.png';
+    let afbeelding = base64String;
+
+    let url = 'https://scrumserver.tenobe.org/scrum/api/image/upload.php';
+
+    let data = {
+        naam: naam,
+        afbeelding: afbeelding
+    }
+
+    var request = new Request(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    });
+
+    fetch(request)
+        .then(function (resp) {                     return resp.json(); })
+        .then(function (data) {
+            console.log('     ==> OK (Foto te vinden op url = ' + data.fileURL + ')');
+            console.log('     • Foto inladen in IMG');
+            //document.getElementById('uploadResult').src = data.fileURL;
+            console.log('     ==> OK');
+            console.log('==> Klaar');
+        })
+        .catch(function (error) { console.log(error); });
+}
+
 document.getElementById('register').addEventListener('click', function (e) {
 
-    var file = document.querySelector('input[type=file]').files[0];
-	var reader = new FileReader();
-  reader.onloadend = function() {
+    let file = document.querySelector('input[type=file]').files[0];
+	let reader = new FileReader();
+    reader.onloadend = function() {
 		console.log('RESULT', reader.result);
-		uploadPicture(reader.result);
-			
+		uploadPicture(reader.result);			
   }
   reader.readAsDataURL(file);
 
-               let data = {
-                   familienaam: document.getElementById('achternaam').value,
-                   voornaam: document.getElementById('voornaam').value,
-                   geboortedatum:document.getElementById('geboortedatum').value,
-                   email: document.getElementById('email').value,
-                   nickname: document.getElementById('nickname').value,
-                   foto: 'test.jpg',
-                   beroep: document.getElementById('beroep').value,
-                   sexe: document.getElementById('geslacht').value,
-                   haarkleur: document.getElementById('haarkleur').value,
-                   oogkleur: document.getElementById('oogkleur').value,
-                   grootte: document.getElementById('grootte').value,
-                   gewicht: document.getElementById('gewicht').value,
-                   wachtwoord: document.getElementById('wachtwoord').value,
-                   lovecoins: 3
-               }
-               var request = new Request(url, {
-                    method: 'POST',
-                    body: JSON.stringify(data),
-                    headers: new Headers({
-                        'Content-Type': 'application/json'
-                    })
-                });
-                fetch(request)
-                                   .then( function (resp)  { return resp.json(); })
-                                   .then( function (data)  { console.log(data);  })
-                                   .catch(function (error) { console.log(error); });
+    let data = {
+        familienaam: document.getElementById('achternaam').value,
+        voornaam: document.getElementById('voornaam').value,
+        geboortedatum:document.getElementById('geboortedatum').value,
+        email: document.getElementById('email').value,
+        nickname: document.getElementById('nickname').value,
+        foto: document.getElementById('foto').value,
+        beroep: document.getElementById('beroep').value,
+        sexe: document.getElementById('geslacht').value,
+        haarkleur: document.getElementById('haarkleur').value,
+        oogkleur: document.getElementById('oogkleur').value,
+        grootte: document.getElementById('grootte').value,
+        gewicht: document.getElementById('gewicht').value,
+        wachtwoord: document.getElementById('wachtwoord').value,
+        lovecoins: 3
+    }
+    var request = new Request(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    });
+    fetch(request)
+        .then( function (resp)  { return resp.json(); })
+        .then( function (data)  { console.log(data);  })
+        .catch(function (error) { console.log(error); });
 
-                           });
+    });
