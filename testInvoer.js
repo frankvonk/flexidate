04aca3dcdd692a699f9e9f2dc8b7bcfb31ead83e
Wout Wabbes <Wout.Wabbes@ad.core.vdabantwerpen.intra>
2019-05-10 10:43:40 +0200

    BUFGIX INSCHRIJVEN

--- testInvoer.js
+++ testInvoer.js
@@ -1,5 +1,3 @@
-const rooturl = "https://scrumserver.tenobe.org/scrum/api";
-let url=rooturl+'/profiel/create.php';
 
 function uploadPicture(base64String) {
     console.log('     • Foto wordt doorgestuurd naar de API.');
@@ -31,44 +29,62 @@ function uploadPicture(base64String) {
             console.log('==> Klaar');
         })
         .catch(function (error) { console.log(error); });
-}
+} // einde functie
 
 document.getElementById('register').addEventListener('click', function (e) {
 
     let file = document.querySelector('input[type=file]').files[0];
-	let reader = new FileReader();
+	  let reader = new FileReader();
     reader.onloadend = function() {
 		console.log('RESULT', reader.result);
-		uploadPicture(reader.result);			
+		uploadPicture(reader.result);
   }
   reader.readAsDataURL(file);
 
-    let data = {
-        familienaam: document.getElementById('achternaam').value,
-        voornaam: document.getElementById('voornaam').value,
-        geboortedatum:document.getElementById('geboortedatum').value,
-        email: document.getElementById('email').value,
-        nickname: document.getElementById('nickname').value,
-        foto: document.getElementById('foto').value,
-        beroep: document.getElementById('beroep').value,
-        sexe: document.getElementById('geslacht').value,
-        haarkleur: document.getElementById('haarkleur').value,
-        oogkleur: document.getElementById('oogkleur').value,
-        grootte: document.getElementById('grootte').value,
-        gewicht: document.getElementById('gewicht').value,
-        wachtwoord: document.getElementById('wachtwoord').value,
-        lovecoins: 3
-    }
-    var request = new Request(url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: new Headers({
-            'Content-Type': 'application/json'
-        })
-    });
-    fetch(request)
-        .then( function (resp)  { return resp.json(); })
-        .then( function (data)  { console.log(data);  })
-        .catch(function (error) { console.log(error); });
+    const rooturl = "https://scrumserver.tenobe.org/scrum/api";
+    let url=rooturl+'/profiel/create.php';
 
-    });
+    let waardeGeslacht = document.getElementById('geslacht').value;
+    console.log(waardeGeslacht);
+    let charGeslacht = "o"
+    switch (waardeGeslacht) {
+      case "man":
+         charGeslacht="m";
+        break;
+      case "vrouw":
+        charGeslacht="v";
+        break;
+      default:
+      charGeslacht = "x";
+    }
+  console.log(charGeslacht);
+  console.log(typeof charGeslacht);
+        let data = {
+            familienaam: document.getElementById('achternaam').value,
+            voornaam: document.getElementById('voornaam').value,
+            geboortedatum:document.getElementById('geboortedatum').value,
+            email: document.getElementById('email').value,
+            nickname: document.getElementById('nickname').value,
+            foto: "no_picture.jpg",
+            beroep: document.getElementById('beroep').value,
+            sexe: charGeslacht,
+            haarkleur: document.getElementById('haarkleur').value,
+            oogkleur: document.getElementById('oogkleur').value,
+            grootte: document.getElementById('grootte').value,
+            gewicht: document.getElementById('gewicht').value,
+            wachtwoord: document.getElementById('wachtwoord').value,
+            lovecoins: "3"
+        }
+        var request = new Request(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        });
+        fetch(request)
+            .then( function (resp)  { return resp.json(); })
+            .then( function (data)  { console.log(data);  })
+            .catch(function (error) { console.log(error); });
+
+        });// Einde knop
