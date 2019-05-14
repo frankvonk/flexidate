8818c460282069ce7d9af72a20154816e3bddc82
Wout Wabbes <Wout.Wabbes@ad.core.vdabantwerpen.intra>
2019-05-14 11:15:33 +0200

    Refracturing + cleanup GitHub

--- authenticate.js
+++ authenticate.js
@@ -1,4 +1,4 @@
-document.getElementById('loginbtn').addEventListener('click', function (e) {
+document.getElementById('loginbtn').addEventListener('click', function (e) { // klick op de login knop
   let nickname = document.getElementById('aanmeldnaam').value;
   let wachtwoord = document.getElementById('wachtwoordinv').value;
 
@@ -24,12 +24,12 @@ document.getElementById('loginbtn').addEventListener('click', function (e) {
     fetch(request)
     .then(function (resp) { return resp.json(); })
     .then(function (data) {
-      if (data.message == 'Authorized') {
+      if (data.message == 'Authorized') { // Als het inloggen slaagt
         console.log("Reactie van backend API : Correcte gegevens");
         localStorage.setItem('profielId',data.id);
         window.location.href = "profiel.html";
         console.log(data.id);
-      } else {
+      } else { // Als het inloggen niet slaagt
         alert("VERKEERDE INLOGGEGEVENS!");
         console.log("Reactie van backend API : Verkeerde gegevens");
       }
